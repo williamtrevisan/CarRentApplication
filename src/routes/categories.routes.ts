eb7c5250c14f0a144880204da42630f3b7799a1b
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-01 20:39:07 -0300

    Realizado refatoração do código utilizando como base alguns princípios de SOLID, como: Responsabilidade Única e Inversão de Dependência

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from '../services/CreateCategoryService';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,12 +10,8 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-    if (categoryAlreadyExists) {
-        return response.status(400).json({ error: 'Category already exists!'});
-    }
-
-    categoriesRepository.create({ name, description });
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({ name, description });
 
     return response.status(201).send();
 });
