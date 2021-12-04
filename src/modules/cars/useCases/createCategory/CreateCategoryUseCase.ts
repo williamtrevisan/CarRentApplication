931ce7e66613aa8e92528326a0386ab0e73bb256
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-04 20:09:07 -0300

    Inclusão do docker na aplicação

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,21 +1,21 @@
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
-    name: string;
-    description: string;
+  name: string;
+  description: string;
 }
 
 class CreateCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    execute({ name, description }: IRequest): void {
-        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
-        if (categoryAlreadyExists) {
-            throw new Error('Category already exists!');
-        }
-
-        this.categoriesRepository.create({ name, description });
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists!");
     }
+
+    this.categoriesRepository.create({ name, description });
+  }
 }
 
 export { CreateCategoryUseCase };
