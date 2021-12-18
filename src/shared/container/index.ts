4ba94a80a5bc2a87b0d6d960f85b345c65f92831
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-18 19:23:45 -0300

    Ajustes de prettier dentro do arquivo do tsyringe

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,12 +2,12 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
-import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
-import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -24,7 +24,4 @@ container.registerSingleton<IUsersRepository>(
   UsersRepository
 );
 
-container.registerSingleton<ICarsRepository>(
-  "CarsRepository",
-  CarsRepository
-);
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
