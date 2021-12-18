36d7fcc7188405000fb95d1d16becc086e36e8e0
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-18 20:38:19 -0300

    Implementação de criação de um seed para criação de usuário admin

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,20 @@
+import { hash } from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidV4();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `
+      INSERT INTO USERS(id, name, password, email, driver_license, admin, created_at)
+      values('${id}', 'admin', '${password}', 'admin@rental.com.br', 'XXXXXXXXX', true, 'now()');
+    `
+  );
+}
+
+create().then(() => console.log("User admin created!"));
