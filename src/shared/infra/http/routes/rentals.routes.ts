194d0386fb3700da248d7eb7be84074be04b3b28
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-19 15:20:49 -0300

    Include devolution route in rentals.routes

---
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+
+const rentalsRoutes = Router();
+
+const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
+
+rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalsRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
+
+export { rentalsRoutes };
