e46b39942f6e77dad170421f140f6a9f1d831ead
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-19 16:05:32 -0300

    Include listRentalsByUser route in rentals routes

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -4,13 +4,16 @@ import { CreateRentalController } from "@modules/rentals/useCases/createRental/C
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalsRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
+rentalsRoutes.get("/user", ensureAuthenticated, listRentalsByUserController.handle);
 
 export { rentalsRoutes };
