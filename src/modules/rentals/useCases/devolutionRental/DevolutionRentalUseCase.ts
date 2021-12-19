f5d989197c84a1a4ff3c57bc1b7f4deacd49fadb
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-19 15:01:43 -0300

    Created DevolutionRentalUseCase

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -23,10 +23,39 @@ class DevolutionRentalUseCase {
   ) {}
 
   async execute({ id, user_id }: IRequest): Promise<void> {
+    const minimumRentalTime = 1;
+    let total = 0;
+
     const rental = await this.rentalsRepository.findByPk(id);
     if (!rental) {
       throw new AppError("Rental does not exists!");
     }
+
+    const car = await this.carsRepository.findByPk(rental.car_id);
+
+    let daily = this.dateProvider.compareInDays(
+      rental.start_date,
+      this.dateProvider.dateNow()
+    );
+    if (daily < minimumRentalTime) {
+      daily = minimumRentalTime;
+    }
+
+    const delay = this.dateProvider.compareInDays(
+      this.dateProvider.dateNow(),
+      rental.expected_return_date
+    );
+    if (delay > 0) {
+      total = delay * car.fine_amount;
+    }
+
+    total += daily * car.daily_rate;
+    
+    rental.end_date = this.dateProvider.dateNow();
+    rental.total = total;
+
+    await this.rentalsRepository.create(rental);
+    await this.carsRepository.updateAvailable(car.id, true);
   }
 }
 
