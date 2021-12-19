f944753a842b91ac7f11958ae246ee6e5fa7ee2a
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-18 23:04:53 -0300

    Ajustes

--- src/shared/infra/typeorm/migrations/1638993263071-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1638993263071-CreateUsers.ts
@@ -8,7 +8,7 @@ export class CreateUsers1638993263071 implements MigrationInterface {
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
           },
           {
