ba13a4c4680ed250f1580ce1a4328db8d88001d8
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-19 01:45:38 -0300

    Realizado início de implementação da funcionalidade de aluguel de carros

---
+++ src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
@@ -0,0 +1,20 @@
+import { RentalsRepositoryInMemory } from "../repositories/in-memory/RentalsRepositoryInMemory";
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe("Create Rental", () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("should be able to create a new rental", () => {
+    createRentalUseCase.execute({
+      user_id: "12345",
+      car_id: "54321",
+      expected_return_date: new Date(),
+    });
+  });
+});
