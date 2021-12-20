24bd4758a0e144040e97e757cf7910e67e0830ea
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-19 22:18:15 -0300

    Included a addDays method in IDateProvider interface and DayjsDateProvider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -3,6 +3,7 @@ interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
+  addDays(days: number): Date;
 }
 
 export { IDateProvider };
