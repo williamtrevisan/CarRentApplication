e53713528da98335a1d22a3e7bf6f4290bf2684a
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-21 12:00:57 -0300

    Continued creation of SendForgotPasswordMailUseCase

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,8 +1,35 @@
-import { injectable } from "tsyringe";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
+import { v4 as uuidV4 } from "uuid";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
-  async execute(email: string): Promise<void> {}
+  constructor(
+    @inject("UsersRepository")
+    private readonly usersRepository: IUsersRepository,
+
+    @inject("UsersTokensRepository")
+    private readonly usersTokensRepository: IUsersTokensRepository,
+
+    @inject("DayjsDateProvider")
+    private readonly dateProvider: IDateProvider
+  ) {}
+
+  async execute(email: string): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError("User does not exists!");
+    }
+
+    await this.usersTokensRepository.create({
+      refresh_token: uuidV4(),
+      user_id: user.id,
+      expires_date: this.dateProvider.dateNow()
+    });
+  }
 }
 
 export { SendForgotPasswordMailUseCase };
