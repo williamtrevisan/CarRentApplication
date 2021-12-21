52eecc759d5ed9148a819d63b13f102d29195f4b
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-21 14:06:30 -0300

    Finished ResetPasswordUserUseCase

--- src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
@@ -1,6 +1,9 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -12,7 +15,13 @@ interface IRequest {
 class ResetPasswordUserUseCase {
   constructor(
     @inject("UsersTokensRepository")
-    private readonly usersTokensRepository: IUsersTokensRepository
+    private readonly usersTokensRepository: IUsersTokensRepository,
+
+    @inject("DayjsDateProvider")
+    private readonly dateProvider: IDateProvider,
+
+    @inject("UsersRepository")
+    private readonly usersRepository: IUsersRepository
   ) {}
 
   async execute({ token, password }: IRequest): Promise<void> {
@@ -22,6 +31,23 @@ class ResetPasswordUserUseCase {
     if (!userToken) {
       throw new AppError("Token invalid!");
     }
+
+    if (
+      this.dateProvider.compareIfBefore(
+        userToken.expires_date,
+        this.dateProvider.dateNow()
+      )
+    ) {
+      throw new AppError("Token expired!");
+    }
+
+    const user = await this.usersRepository.findByPk(userToken.id);
+
+    user.password = await hash(password, 8);
+
+    await this.usersRepository.create(user);
+
+    await this.usersTokensRepository.deleteByPk(userToken.id);
   }
 }
 
