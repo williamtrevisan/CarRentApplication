0dcc06d0ede526fd83202181baa81c33b53de817
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-21 12:08:59 -0300

    Included register from EtherealMailProvider and IMailProvider in tsyringe and created a base from EtherealMailProvider

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,9 @@
+import { IMailProvider } from "../IMailProvider";
+
+class EtherealMailProvider implements IMailProvider {
+  sendMail(to: string, subject: string, body: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export { EtherealMailProvider };
