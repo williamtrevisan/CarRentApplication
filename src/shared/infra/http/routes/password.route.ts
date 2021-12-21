cc0f9fb8b33ad56cde817ec05552b13b04aa41e3
William Trevisan <william.trevisan@multi24h.com.br>
2021-12-21 14:09:00 -0300

    Created reset route in password routes

--- src/shared/infra/http/routes/password.route.ts
+++ src/shared/infra/http/routes/password.route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordMailController = new ResetPasswordUserController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetPasswordMailController.handle);
 
 export { passwordRoutes };
